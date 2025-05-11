45819b36e43566bdb569fd43ff5d0aea63cbd22b
cs4151 <hannah.gabele@student.uibk.ac.at>
2025-05-11 20:21:45 +0200

    Pop up für die wetter stationen

--- main.js
+++ main.js
@@ -53,7 +53,13 @@ async function loadStations(url) {
             });
         },
         onEachFeature: function (feature, layer) {
-
+            layer.bindPopup(`
+                <h4>${feature.properties.name} (${feature.geometry.coordinates[2]}m)</h4>
+            `);
+        }
+    }).addTo(overlays.stations)
+}
+loadStations("https://static.avalanche.report/weather_stations/stations.geojson");
             //information zu seehöhe aus drittem wert der koordinaten (index 2)
             let elevation = feature.geometry.coordinates.length > 2
                 ? (feature.geometry.coordinates[2])
